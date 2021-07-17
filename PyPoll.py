4b7f87c3cfc0f4dadde5b76c6bed5ad5483f595f
David Szymkowiak <davidszymkowiak@Davids-MA.local>
2021-07-17 11:43:30 -0700

    Adding updated pypoll code and updated txt file

--- PyPoll.py
+++ PyPoll.py
@@ -43,13 +43,27 @@ with open(file_to_load) as election_data: # Opens a file without using open/clos
 
             candidate_votes[candidate_name] = 0 # Set the candidates votes to 0
         candidate_votes[candidate_name] += 1 # Add 1 vote each time a candidate name appears
-        
+   
+with open(file_to_save, "w") as txt_file:  
+
+    election_results = (
+        f"\nElection Results\n"
+        f"-------------------------\n"
+        f"Total Votes: {total_votes:,}\n"
+        f"-------------------------\n")
+    print(election_results, end="")
+    txt_file.write(election_results)
+
     for candidate_name in candidate_votes:
         votes = candidate_votes[candidate_name]
 
         vote_percentage = float(votes)/float(total_votes) * 100
 
-        print(f"\n{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+        candidate_results = (
+            f"\n{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+
+        print(candidate_results)
+        txt_file.write(candidate_results)
 
         if (votes > winning_count) and (vote_percentage > winning_percentage):
             # If true then set winning_count = votes and winning_percent =
@@ -59,18 +73,12 @@ with open(file_to_load) as election_data: # Opens a file without using open/clos
             # And, set the winning_candidate equal to the candidate's name.
             winning_candidate = candidate_name
 
-winning_candidate_summary = (
-    f"-------------------------\n"
-    f"Winner: {winning_candidate}\n"
-    f"Winning Vote Count: {winning_count:,}\n"
-    f"Winning Percentage: {winning_percentage:.1f}%\n"
-    f"-------------------------\n")
-print(winning_candidate_summary)
+    winning_candidate_summary = (
+        f"-------------------------\n"
+        f"Winner: {winning_candidate}\n"
+        f"Winning Vote Count: {winning_count:,}\n"
+        f"Winning Percentage: {winning_percentage:.1f}%\n"
+        f"-------------------------\n")
+    print(winning_candidate_summary)
+    txt_file.write(winning_candidate_summary)
 
-
-
-# open and write to election_analysis
-with open(file_to_save, 'w') as txt_file:
-    txt_file.write("Counties in the Election")
-    txt_file.write('\n-----------------------------')
-    txt_file.write("\nArapahoe\nDenver\nJefferson")
